e5101923d7fe5af1d21e27c2e1091b04fa016774
Dag Odenhall <dag.odenhall@gmail.com>
2011-02-25 00:30:11 +0100

    Thanks Chris

--- Haskell/4.hs
+++ Haskell/4.hs
@@ -1,11 +1,10 @@
 import Data.List
 
 palindromes = filter palindromic
-            . map pairProduct
+            . map (uncurry (*))
             . concatMap (combine nums) $ nums
   where
     palindromic x     = show x == reverse (show x)
-    pairProduct (x,y) = x * y
     combine x y       = zip (repeat y) x
     nums              = [100 .. 999]
 
